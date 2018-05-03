5d2226e6291aab809121f46f9697bdbfe795dc18
xxy <xxy@xiangyang.com>
2018-05-03 17:50:38 +0800

    add resp code check

--- lib/thrift_rack/http_client_transport.rb
+++ lib/thrift_rack/http_client_transport.rb
@@ -4,6 +4,7 @@ require "stringio"
 Excon.defaults[:ssl_verify_peer] = false
 class ThriftRack
   class HttpClientTransport < Thrift::BaseTransport
+    class RespCodeError < StandardError; end
     def initialize(client, path, opts = {})
       @headers = {'Content-Type' => 'application/x-thrift'}
       @outbuf = Thrift::Bytes.empty_byte_buffer
@@ -23,6 +24,7 @@ class ThriftRack
     def flush
       resp = @client.post(path: @path, body: @outbuf, headers: @headers, idempotent: true)
       data = resp.body
+      raise RespCodeError.new("#{resp.status} on #{@path} with body #{data}") unless resp.status == 200
       data = Thrift::Bytes.force_binary_encoding(data)
       @inbuf = StringIO.new data
     ensure
--- lib/thrift_rack/version.rb
+++ lib/thrift_rack/version.rb
@@ -1,3 +1,3 @@
 class ThriftRack
-  VERSION = "0.1.8"
+  VERSION = "0.1.9"
 end
