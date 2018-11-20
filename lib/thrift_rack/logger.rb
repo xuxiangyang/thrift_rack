36e916faa269605116678f927315088f00c75d30
xuxiangyang <xxy@creatingev.com>
2018-11-20 15:31:25 +0800

    change logger

--- lib/thrift_rack/logger.rb
+++ lib/thrift_rack/logger.rb
@@ -13,35 +13,31 @@ class ThriftRack
       resp
     ensure
       end_time = Time.now
-      self.logger.info(JSON.dump({
-        request_at: request_at.iso8601(6),
-        request_id: req.env["HTTP_X_REQUEST_ID"],
-        rpc_id: req.env["HTTP_X_RPC_ID"],
-        duration: ((end_time - request_at) * 1000).round(4),
-        path: req.path,
-        func: req.env["HTTP_X_RPC_FUNC"],
-        from: req.env["HTTP_X_FROM"],
-        tag: Logger.tag,
-      }))
-    end
-
-    def logger
-      @logger ||= (defined? Rails) ? rails_logger : std_logger
-    end
-
-    private
-
-    def rails_logger
-      file = File.open("#{Rails.root}/log/rpc.log", File::WRONLY | File::APPEND | File::CREAT)
-      ActiveSupport::Logger.new(file)
-    end
-
-    def std_logger
-      ::Logger.new(STDOUT)
+      self.logger.info(
+        JSON.dump(
+          request_at: request_at.iso8601(6),
+          request_id: req.env["HTTP_X_REQUEST_ID"],
+          rpc_id: req.env["HTTP_X_RPC_ID"],
+          duration: ((end_time - request_at) * 1000).round(4),
+          path: req.path,
+          func: req.env["HTTP_X_RPC_FUNC"],
+          from: req.env["HTTP_X_FROM"],
+          tag: Logger.tag,
+        ),
+      )
     end
 
     class << self
       attr_writer :tag
+
+      def logger
+        @logger ||= if defined? Rails
+                      ActiveSupport::Logger.new(File.open("#{Rails.root}/log/rpc.log", File::WRONLY | File::APPEND | File::CREAT))
+                    else
+                      ::Logger.new(STDOUT)
+                    end
+      end
+
       def tag
         @tag ||= {}
       end
