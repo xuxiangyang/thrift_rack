4075dbdb3ce9d1a35d5cd270706191d5f74836f2
xuxiangyang <xxy@xuxiangyang.com>
2020-03-25 15:06:28 +0800

    fix bug last time

--- lib/thrift_rack/http_client_transport.rb
+++ lib/thrift_rack/http_client_transport.rb
@@ -68,7 +68,7 @@ class ThriftRack
       end
 
       def new_http(name, max_requests: 100)
-        http = Net::HTTP::Persistent.new(name)
+        http = Net::HTTP::Persistent.new(name: name)
         http.retry_change_requests = true
         http.max_requests = max_requests
         http.verify_mode = 0
